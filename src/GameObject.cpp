a30bb02ebe24db30b4099eb217bb82a6abf09e70
Alberto La Scala <albelax94@gmail.com>
2016-04-27 11:55:46 +0100

    Update GameObject.cpp

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,4 +1,4 @@
-#include "gameobject.h"
+#include "GameObject.h"
 
 GameObject::GameObject(const Vec4 _position, const Vec4 _rotation, float _speed = 0, bool _active = true) :
 m_position(_position), m_rotation(_rotation), m_speed(_speed = 0.1f), m_active(_active = true)
